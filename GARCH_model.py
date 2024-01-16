489a9001f637d35ff6b2d58578b1be7124a35476
ivy <35571070+ivywang5858@users.noreply.github.com>
2024-01-15 23:21:27 -0500

    GARCH More Models

--- GARCH_model.py
+++ GARCH_model.py
@@ -21,8 +21,46 @@ basic_gm = arch_model(sp_data['Return'], p =1, q = 1, mean = 'constant', vol = '
 # gm_result.plot()
 # plt.show()
 
-#GARCH MODEL Forecast
-gm_result = basic_gm.fit()
+#GARCH Model Forecast
+gm_result = basic_gm.fit(disp= 'off')
 gm_forecast = gm_result.forecast(horizon = 5)
-print(gm_forecast.variance[-1:])
+# print(gm_forecast.variance[-1:])
 
+#Mean Model Specifications - mean uses AR model
+constant_mean_result = arch_model(sp_data['Return'],p =1, q = 1, mean = 'constant', vol = 'GARCH')
+constant_mean_model = constant_mean_result.fit(disp= 'off')
+# print(constant_mean_model.summary())
+cmean_vol = constant_mean_model.conditional_volatility
+
+ar_mean_result = arch_model(sp_data['Return'],p =1, q = 1, mean = 'AR', vol = 'GARCH')
+ar_mean_model = ar_mean_result.fit(disp= 'off')
+# print(ar_mean_model.summary())
+armean_vol = ar_mean_model.conditional_volatility
+
+# plt.plot(cmean_vol,color = 'blue', label = 'Constant Mean Volatility')
+# plt.plot(armean_vol,color = 'red', label = 'AR Mean Volatility')
+# plt.legend(loc = 'upper right')
+# plt.show()
+# print(np.corrcoef(cmean_vol,armean_vol)[0,1])
+
+#Volatility Models for asymmetric shocks - GJR GARCH & EGARCH
+gjr_gm = arch_model(sp_data['Return'],p =1, q = 1, o = 1, vol = 'GARCH', dist = 't' )
+gjr_result = gjr_gm.fit(disp = 'off')
+# print(gjr_result.summary())
+gjrgm_vol = gjr_result.conditional_volatility
+
+egarch_gm = arch_model(sp_data['Return'],p =1, q = 1, o = 1, vol = 'EGARCH', dist = 't' )
+egarch_result = egarch_gm.fit(disp = 'off')
+# print(egarch_result.summary())
+egarch_vol = egarch_result.conditional_volatility
+
+# plt.plot(sp_data['Return'], color = 'grey',alpha = 0.4, label = 'Price Returns')
+# plt.plot(gjrgm_vol, color = 'gold', label = 'GJR-GARCH Volatility')
+# plt.plot(egarch_vol, color = 'red', label = 'EGARCH Volatility')
+# plt.legend(loc = 'upper right')
+# plt.show()
+
+# GARCH Rolling Window Forecast
+basic_gm = arch_model(sp_data['Return'], p =1, q = 1, mean = 'constant', vol = 'GARCH', dist = 'normal')
+for i in range(30):
+    gm_result = basic_gm.fit(first_obs= i+1000, last_obs= i+1500, update_freq = 5)
